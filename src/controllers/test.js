e6e8009fada7c07aa94bdc56027b25247fed5b52
Vinayak Shrivastava <vinayak.shrivastava@fibonalabs.com>
2021-03-28 03:42:27 +0530

    image resize service using api call implemented

--- src/controllers/test.js
+++ src/controllers/test.js
@@ -1,10 +1,20 @@
-import { TESTS } from '../db/models';
+import Jimp from 'jimp'
 
 export async function test(req, res) {
     try {
 
+        const rootFolder = `./assets/`
+
+        const imagePath = `${rootFolder}/originalImage/download.jpg`
+
+        const readImage = await Jimp.read(imagePath)
+        
+        const resizedImage = readImage
+                            .resize(140, 140) // resize
+                            .write(`${rootFolder}/resizedImage/download-resized.jpg`); // save
+  
         return res.status(200).json({ 
-            msg: `TEST SUCCESS` 
+            msg: true 
         });
     
     } catch (e) {
